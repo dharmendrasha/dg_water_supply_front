aa1711783653532fa1b1d8c6bf95f48765cbfa87
sumit-kr-das <sumitkrdas777@gmail.com>
2022-03-02 20:18:21 +0530

    home page complete

--- script/main.script.js
+++ script/main.script.js
@@ -1,4 +1,20 @@
-$("#menu-toggle").click(function(e) {
-    e.preventDefault();
-    $("#wrapper").toggleClass("toggled");
-  });
+AOS.init();
+
+const clickHamButton = document.querySelector('.hamburger-btn');
+const showMenu = document.querySelector('.drawer');
+const shadow = document.querySelector('.bg-shadow')
+
+clickHamButton.addEventListener('click', () => {
+    const isAdd = showMenu.classList.contains('show');
+    if(isAdd){
+        showMenu.classList.remove('show');
+    }else{
+        showMenu.classList.add('show');
+    }
+    shadow.classList.toggle('remove-shadow');
+});
+
+shadow.addEventListener('click', () => {
+    showMenu.classList.remove('show');
+    shadow.classList.toggle('remove-shadow');
+})
