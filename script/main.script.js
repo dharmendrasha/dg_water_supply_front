4d43711ae5f1d948695a5980e32423f43c3274c5
sumit-kr-das <sumitkrdas777@gmail.com>
2022-03-04 11:18:12 +0530

    form submission completed

--- script/main.script.js
+++ script/main.script.js
@@ -1,36 +1,35 @@
 AOS.init();
 
-
-// drop down
-
-function show(value) {
-    document.querySelector(".text-box").value = value;
-  }
-  
-  let dropdown = document.querySelector(".dropdown")
-  dropdown.onclick = function() {
-      dropdown.classList.toggle("active")
-  }
-  
-
-
 // mobile nav
-const clickHamButton = document.querySelector('.hamburger-btn');
-const showMenu = document.querySelector('.drawer');
-const shadow = document.querySelector('.bg-shadow')
+const clickHamButton = document.querySelector(".hamburger-btn");
+const showMenu = document.querySelector(".drawer");
+const shadow = document.querySelector(".bg-shadow");
 
-clickHamButton.addEventListener('click', () => {
-    const isAdd = showMenu.classList.contains('show');
-    if(isAdd){
-        showMenu.classList.remove('show');
-    }else{
-        showMenu.classList.add('show');
-    }
-    shadow.classList.toggle('remove-shadow');
+clickHamButton.addEventListener("click", () => {
+  const isAdd = showMenu.classList.contains("show");
+  if (isAdd) {
+    showMenu.classList.remove("show");
+  } else {
+    showMenu.classList.add("show");
+  }
+  shadow.classList.toggle("remove-shadow");
 });
 
-shadow.addEventListener('click', () => {
-    showMenu.classList.remove('show');
-    shadow.classList.toggle('remove-shadow');
-})
+shadow.addEventListener("click", () => {
+  showMenu.classList.remove("show");
+  shadow.classList.toggle("remove-shadow");
+});
 
+
+// show and hide daily & frequently
+
+const dailyClick = document.querySelector('.daily-click')
+const frequentlyClick = document.querySelector('.frequently-click')
+const dailyBasis = document.querySelector('.show-daily');
+const frequentlyBasis = document.querySelector('.show-frequently');
+
+// hidebasis
+dailyClick.addEventListener('click', () => {
+  console.log(dailyBasi);
+  dailyBasis.classList.add('hidebasis')
+})
